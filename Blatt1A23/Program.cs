821f71c3d64b42917510532dab0db829580920e1
Dayeeet <david@cappell.net>
2024-04-14 21:01:08 +0200

    add: potenz methods

---
+++ Blatt1A23/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Blatt1A23;
+
+public class Program
+{
+    public static double Potenz(double x, int n)
+    {
+        var value = 1.0;
+        for (int i = 0; i < n; i++)
+        {
+            value *= x;
+        }
+
+        return value;
+    }
+
+    public static double PotenzRek(double x, int n)
+    {
+        if (n <= 0) return 1;
+        return x * PotenzRek(x, n - 1);
+    }
+
+    public static void Main(string[] args)
+    {
+        
+    }
+}
