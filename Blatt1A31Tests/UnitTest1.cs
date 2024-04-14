670b975b29d214d1d96e46e3136756835be80c4c
SeanNober <sean.nober@gmail.com>
2024-04-14 22:31:55 +0200

    Sollte passen abfahrt

---
+++ Blatt1A31Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+
+using Blatt1A31;
+using Program = Microsoft.VisualStudio.TestPlatform.TestHost.Program;
+
+namespace Blatt1A31Tests;
+
+public class Tests
+{
+
+    
+    //So'n Test Ding is hier unnötig weil bro der Code geht halt
+}
